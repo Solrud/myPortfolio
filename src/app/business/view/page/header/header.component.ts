04c3cffc7392010ac782a5dfc7fb5cb169694c62
ilin-mi <ilin-mi@avid.ru>
2023-08-16 17:00:00 +0500

    какой никакой лендинг 1, 2 страницы показывается, верстка через flex

--- src/app/business/view/page/header/header.component.ts
+++ src/app/business/view/page/header/header.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ViewportScroller} from "@angular/common";
 
 @Component({
   selector: 'app-header',
@@ -7,13 +8,17 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit{
 
+  constructor(
+    private scroller: ViewportScroller)
+  {}
+
   ngOnInit(): void {
-    console.log(document.getElementById('skills'));
+
   }
 
-  scroll(el: HTMLElement) {
-    el.scrollIntoView({behavior: 'smooth'});
-  }
 
+  scrollToSection(sectionID: string) {
+    this.scroller.scrollToAnchor(sectionID)
+  }
 
 }
--- src/app/business/view/page/inner-body/inner-body.component.ts
+++ src/app/business/view/page/inner-body/inner-body.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ViewportScroller} from "@angular/common";
+import {RESUME_CARDS} from "../../../../app.constant";
 
 @Component({
   selector: 'app-inner-body',
   templateUrl: './inner-body.component.html',
   styleUrls: ['./inner-body.component.css']
 })
-export class InnerBodyComponent {
+export class InnerBodyComponent implements OnInit{
+    resumeCards = RESUME_CARDS;
+
+  ngOnInit(): void {
+    console.log()
+  }
 
 }
